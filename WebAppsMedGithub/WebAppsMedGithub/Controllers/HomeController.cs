0bfd32230ec0774b4307afc7d64030f08e575769
Viktor Danielsen <viktordan9019@gmail.com>
2018-09-26 16:23:33 +0200

    tesssst

--- WebAppsMedGithub/WebAppsMedGithub/Controllers/HomeController.cs
+++ WebAppsMedGithub/WebAppsMedGithub/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WebAppsMedGithub.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        // GET: Home testest
         public ActionResult Index()
         {
             return View();
