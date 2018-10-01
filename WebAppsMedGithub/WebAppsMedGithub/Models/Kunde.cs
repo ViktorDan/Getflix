46f4c215fc3132f1838c9d6dd56ea903522e5474
Kristian Ludvig Grønvold <s315691@oslomet.no>
2018-10-01 19:45:33 +0200

    La til validerin g, funker ikke ennå

--- WebAppsMedGithub/WebAppsMedGithub/Models/Kunde.cs
+++ WebAppsMedGithub/WebAppsMedGithub/Models/Kunde.cs
@@ -8,14 +8,42 @@ namespace WebAppsMedGithub.Models
 {
     public class Kunde
     {
+        [Display(Name = "Brukernavn")]
+        [Required(ErrorMessage = "Må oppgi ønsket brukernavn")]
+        [RegularExpression (@"[a-zæøåA-ZÆØÅ0-9 ]{2,15}", ErrorMessage = "Feil i brukernavn")]
         public string Brukernavn { get; set; }
+
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Må oppgi fornavn")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,30}", ErrorMessage = "Feil i fornavn")]
         public string Fornavn { get; set; }
+
+        [Display(Name = "Etternavn")]
+        [Required(ErrorMessage = "Må oppgi etternavn")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,15}", ErrorMessage = "Feil i etternavn")]
         public string Etternavn { get; set; }
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Må oppgi adresse")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ0-9 ]{4,20}", ErrorMessage = "Feil i adresse")]
         public string Adresse { get; set; }
+
+        [Display(Name = "Postnummer")]
+        [Required(ErrorMessage = "Må oppgi postnummer")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Feil i brukernavn")]
         public string Postnr { get; set; }
+
+        [Display(Name = "Telefonnummer")]
+        [Required(ErrorMessage = "Må oppgi telefonnummer")]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = "Feil i Telefonnummer")]
         public string Tlf { get; set; }
 
+
+        [Display(Name = "Passord")]
+        [Required(ErrorMessage = "Må oppgi ønsket passord")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ0-9 ]{2,15}", ErrorMessage = "Feil i passord")]
         public string Passord { get; set; }
+        
 
         //public string Etternavn { get; set; }
         //public string Adresse { get; set; }
