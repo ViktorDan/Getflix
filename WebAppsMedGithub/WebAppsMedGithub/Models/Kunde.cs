19bb4edce3bbb002489607bc428cf9e1292b8612
Kristian Ludvig Grønvold <s315691@oslomet.no>
2018-10-07 15:17:07 +0200

    as

--- WebAppsMedGithub/WebAppsMedGithub/Models/Kunde.cs
+++ WebAppsMedGithub/WebAppsMedGithub/Models/Kunde.cs
@@ -16,43 +16,43 @@ namespace WebAppsMedGithub.Models
 
         [Display(Name = "Brukernavn")]
         [Required(ErrorMessage = "Må oppgi ønsket brukernavn")]
-        [RegularExpression (@"[a-zæøåA-ZÆØÅ0-9 ]{2,15}", ErrorMessage = "Feil i brukernavn")]
+        [RegularExpression (@"[a-zæøåA-ZÆØÅ0-9 ]{2,15}", ErrorMessage = "Ugyldig brukernavn")]
         public string Brukernavn { get; set; }
 
         [Display(Name = "Fornavn")]
         [Required(ErrorMessage = "Må oppgi fornavn")]
-        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,30}", ErrorMessage = "Feil i fornavn")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,30}", ErrorMessage = "Ugyldig fornavn")]
         public string Fornavn { get; set; }
 
         [Display(Name = "Etternavn")]
         [Required(ErrorMessage = "Må oppgi etternavn")]
-        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,15}", ErrorMessage = "Feil i etternavn")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,15}", ErrorMessage = "Ugyldig etternavn")]
         public string Etternavn { get; set; }
 
         [Display(Name = "Adresse")]
         [Required(ErrorMessage = "Må oppgi adresse")]
-        [RegularExpression(@"[a-zæøåA-ZÆØÅ0-9 ]{4,20}", ErrorMessage = "Feil i adresse")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ0-9 ]{4,20}", ErrorMessage = "Ugyldig adresse")]
         public string Adresse { get; set; }
 
         [Display(Name = "Postnummer")]
         [Required(ErrorMessage = "Må oppgi postnummer")]
-        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Feil i postnummer")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Ugyldig postnummer")]
         public string Postnr { get; set; }
 
         [Display(Name = "Poststed")]
         [Required(ErrorMessage = "Må oppgi poststed")]
-        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,30}", ErrorMessage = "Feil i poststed")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ -]{2,30}", ErrorMessage = "Ugyldig poststed")]
         public string Poststed { get; set; }
 
         [Display(Name = "Telefonnummer")]
         [Required(ErrorMessage = "Må oppgi telefonnummer")]
-        [RegularExpression(@"[0-9]{8}", ErrorMessage = "Feil i Telefonnummer")]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = "Ugyldig Telefonnummer")]
         public string Tlf { get; set; }
 
 
         [Display(Name = "Passord")]
         [Required(ErrorMessage = "Må oppgi ønsket passord")]
-        [RegularExpression(@"[a-zæøåA-ZÆØÅ0-9]{5,15}", ErrorMessage = "Feil i passord")]
+        [RegularExpression(@"[a-zæøåA-ZÆØÅ0-9]{5,15}", ErrorMessage = "Ugyldig passord")]
         public string Passord { get; set; }
     }
 }
