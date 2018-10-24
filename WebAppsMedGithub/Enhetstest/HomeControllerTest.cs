8992f7baad9e718fc4b9cfc03d6362d88ef21bbe
Kristian Ludvig Grønvold <s315691@oslomet.no>
2018-10-24 13:45:41 +0200

    La til testprosjekt

---
+++ WebAppsMedGithub/Enhetstest/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using WebAppsMedGithub.BLL;
+using WebAppsMedGithub.Controllers;
+//using WebAppsMedGithub.DAL;
+using WebAppsMedGithub.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enhetstest
+{
+    [TestClass]
+    class HomeControllerTest
+    {
+        
+    }
+}
---
+++ WebAppsMedGithub/Enhetstest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Enhetstest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Enhetstest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f845efc-2d20-44f0-b570-8c73816783c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
